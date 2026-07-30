c6501ae28931384794817dabde5ebf8c0e31adcc
CWDamm-Kint <chris.damm@k-int.co.uk>
2026-07-30 15:28:47 +0100

    change plugin migration to python

--- arches_keep_app/migrations/0002_add_plugin.py
+++ arches_keep_app/migrations/0002_add_plugin.py
@@ -1,33 +1,39 @@
 from django.db import migrations, models
 
+def add_keep_app_plugin(apps, schema_editor):
+    Plugin = apps.get_model("models", "Plugin")
+
+    if not Plugin.objects.filter(pk="eb53e958-9ddf-40a9-8acd-da2b27df8340").exists():
+        Plugin.objects.update_or_create(
+            pluginid="eb53e958-9ddf-40a9-8acd-da2b27df8340",
+            name={"en": "Keep Integration Dashboard"},
+            icon="fa fa-link",
+            component="views/components/plugins/keep_integration_dashboard",
+            componentname="keep_integration_dashboard",
+            config={
+                "show": True,
+                "is_workflow": False,
+                "description": {"en": None},
+                "i18n_properties": ["description"],
+            },
+            slug="keep_integration_dashboard",
+            sortorder=1,
+        )
+
+def remove_keep_app_plugin(apps, schema_editor):
+    Plugin = apps.get_model("models", "Plugin")
+    keep_integration_plugin = Plugin.objects.get(pk="eb53e958-9ddf-40a9-8acd-da2b27df8340")
+    keep_integration_plugin.delete()
 
 class Migration(migrations.Migration):
-
-    initial = True
 
     dependencies = [
         ("arches_keep_app", "0001_initial")
     ]
 
-    forward = """
-        INSERT INTO plugins (
-            pluginid, name, icon, component, componentname, config, slug, sortorder)
-        VALUES (
-            'eb53e958-9ddf-40a9-8acd-da2b27df8340',
-            'Keep Integration Dashboard',
-            'fa fa-link',
-            'views/components/plugins/keep_integration_dashboard',
-            'keep_integration_dashboard', 
-            '{"show": true, "is_workflow": false, "description": ""}',
-            'keep_integration_dashboard',
-            '1'
-        );
-        """
-    
-    reverse = """
-        DELETE FROM plugins where pluginid = 'eb53e958-9ddf-40a9-8acd-da2b27df8340';
-        """
-
     operations = [
-        migrations.RunSQL(forward, reverse),
+        migrations.RunPython(
+            add_keep_app_plugin,
+            remove_keep_app_plugin,
+        ),
     ]
