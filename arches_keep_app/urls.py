f0bd7e99457271d1de3473e152c8727680cf4dc3
Samuel Scandrett <samuel.scandrett@k-int.co.uk>
2026-09-10 08:36:25 +0100

    Add missing Arches app files, and bump version

--- arches_keep_app/urls.py
+++ arches_keep_app/urls.py
@@ -1,19 +1,8 @@
-"""URL Configuration
+from django.conf import settings
+from django.conf.urls.static import static
+from django.conf.urls.i18n import i18n_patterns
+from django.urls import include, path, re_path
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-from django.urls import path, re_path
 from arches_keep_app.views.keep import ChangesView, ConceptsExportView
 from arches_keep_app.views.keep_export import process_resource
 
@@ -22,3 +11,17 @@ urlpatterns = [
     re_path(r"^concept/export", ConceptsExportView.as_view(), name="ConceptsExportView"),    
     re_path(r"^keep/export/$", process_resource, name='process_resource'),
 ]
+
+# Ensure Arches core urls are superseded by project-level urls
+urlpatterns.append(path("", include("arches.urls")))
+
+# Adds URL pattern to serve media files during development
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+# Only handle i18n routing in active project. This will still handle the routes provided by Arches core and Arches applications,
+# but handling i18n routes in multiple places causes application errors.
+if settings.ROOT_URLCONF == __name__:
+    if settings.SHOW_LANGUAGE_SWITCH is True:
+        urlpatterns = i18n_patterns(*urlpatterns)
+
+    urlpatterns.append(path("i18n/", include("django.conf.urls.i18n")))
